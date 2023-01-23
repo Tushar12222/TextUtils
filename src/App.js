12a4196ecea30f8a4421b7d397720c7c0f24771d
Tushar GH <tusharh75@gmail.com>
2023-01-23 20:58:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+ 
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled" , "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled" , "success");
+
+    }
+  }
+  return (
+    <>
+<Router>
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
+<div className="container">
+<Routes>
+          <Route exact path="/about" element={<About/>}/>
+            
+          
+          <Route exact path="/" element={<TextForm heading="Enter the text below to analyze:" mode={mode} showAlert={showAlert}/>}/>
+
+            
+          
+</Routes>
+
+</div>
+</Router>
+    
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+
+
+
+export default function TextForm(props) {
+  
+    const handleloClick = ()=>{
+      setPrevText(text);
+      let newText = text.toLowerCase();
+      setText(newText);
+      props.showAlert("Converted to LowerCase","success");
+    }  
+    const handleUpClick = ()=>{
+        setPrevText(text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UpperCase","success");
+    }
+    const handleOnChange = (event)=>{
+        setText(event.target.value);
+    }
+    const handleSpaceClick = ()=>{
+        setPrevText(text);
+        let newText = text.split(" ").join("");
+        setText(newText);
+        props.showAlert("Spaces Removed","success");
+    }
+    const handleClearClick = ()=>{
+        setPrevText(text);
+        let newText = "";
+        setText(newText);
+        props.showAlert("Text Cleared","success");
+    }
+    const handleRevClick = ()=>{
+        
+        
+        setText(prevText);
+        props.showAlert("Original Text Recovered","success");
+    }
+    const [text, setText] = useState('');
+    const [prevText, setPrevText] = useState('');
+  return ( 
+    <>
+    
+  <div className="mb-3" style={{color:props.mode === 'dark'? 'white' : 'black'}}>
+    <label htmlFor="myBox"  className="form-label"><h4>{props.heading}</h4></label>
+    <textarea className="form-control" style={{backgroundColor:props.mode === 'dark'? 'grey' : 'white', color:props.mode === 'dark'? 'white' : 'black'}} value={text} onChange={handleOnChange} id="myBox" rows="8"/>
+
+  </div>
+  <button  className="btn btn-primary mx-1 my-1" onClick={handleUpClick} >Convert to UpperCase</button>
+  <button  className="btn btn-primary mx-1 my-1" onClick={handleloClick} >Convert to LowerCase</button>
+  <button  className="btn btn-primary mx-1 my-1" onClick={handleSpaceClick} >Remove Spaces</button>
+  <button  className="btn btn-primary mx-1 my-1" onClick={handleClearClick} >Clear Text</button><br />
+  
+  <button  className="btn btn-primary mx-1 my-1" onClick={handleRevClick} >Revert to Original Text</button>
+
+
+  <div className="container my-2" style={{color:props.mode === 'dark'? 'white' : 'black'}}>
+    <br />
+    <h1>Your text summary:</h1>
+    <p>{text.split(" ").length} words, {text.length} characters, {text.split(".").length} sentences</p>
+    <h2>Preview:</h2>
+    <p>{text.length>0 ? text : "Enter text in the textbox to preview"}</p>
+  </div>
+
+    </>
+  )
+}
